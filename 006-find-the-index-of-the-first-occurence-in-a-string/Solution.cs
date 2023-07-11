994868a0d88c1efa7cb168c391ed28651d56c555
Hamza Kavak <69338731+Glipotions@users.noreply.github.com>
2023-07-12 00:56:28 +0300

    "Find the Index of the First Occurrence in a String" solved

---
+++ 006-find-the-index-of-the-first-occurence-in-a-string/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _006_find_the_index_of_the_first_occurence_in_a_string
+{
+    public class Solution
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            if (haystack.Contains(needle))
+            {
+                var t=haystack.Replace(needle, "#");
+                return t.IndexOf("#");
+            }
+            else
+                return -1;
+        }
+    }
+}
