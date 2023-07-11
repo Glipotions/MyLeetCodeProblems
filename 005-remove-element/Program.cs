da3b28876c322bbfbc213c38da64c8c8cd736888
Hamza Kavak <69338731+Glipotions@users.noreply.github.com>
2023-07-12 00:47:00 +0300

    remove-element solved

---
+++ 005-remove-element/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _005_remove_element;
+
+Console.WriteLine("Hello, World!");
+var sol = new Solution();
+int[] nums = new int[] { 3, 2, 2, 3 };
+Console.WriteLine(sol.RemoveElement(nums, 3));
---
+++ 005-remove-element/Solution.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _005_remove_element
+{
+    public class Solution
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            /// Seçenek 1
+            var t = new List<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    t.Add(nums[i]);
+                }
+            }
+            for (int i = 0; i < t.Count(); i++)
+            {
+                nums[i] = t[i];
+            }
+
+            return t.Count();
+
+            /// Seçenek 2
+            
+            //int j = 0;
+            //foreach (int i in nums)
+            //{
+            //    if (i != val)
+            //    {
+            //        nums[j++] = i;
+            //    }
+            //}
+
+            //return j;
+        }
+
+
+    }
+}
